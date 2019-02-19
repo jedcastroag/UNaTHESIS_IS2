ec0ee4da128a1b111d6578931b46567d0e858e69
Cristhian Camilo Gomez Neira <ccgomezn@unal.edu.co>
2019-02-19 13:27:50 -0500

    codigo dañado

--- app/javascript/components/usersAdministrator.js
+++ app/javascript/components/usersAdministrator.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import jQuery from 'jquery';
-import $ from 'jquery';
-import ReactDOM from 'react-dom'
+
 import TopMenu from './TopMenu';
 import { Container, Header } from 'semantic-ui-react'
 import '../../../dist/semantic.min.css';
-import '../../assets/javascripts/dataTables/datatables.min.js'
-import '../../assets/javascripts/dataTables/datatables.min.css'
 
 
 class UsersAdministrator extends React.Component {
