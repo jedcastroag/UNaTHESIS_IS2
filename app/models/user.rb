c83b1b7f0243efdfc1d7ef58334bdede64111252
Yarid Perez <ayperezc@unal.edu.co>
2019-02-20 21:01:22 -0500

    Mailing y correo a nuevo usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 	validates :name, presence:true, length: { maximum: 60 }
 	validates :password, presence:true, length: { minimum: 8, maximum: 30 }
 	validates :user_type_id, presence:true
+	after_create :send_mail
 	
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence:true, length: { maximum: 254 },
@@ -12,4 +13,11 @@ class User < ApplicationRecord
 	has_and_belongs_to_many :thesis_projects
 	
 	has_secure_password
+
+	private
+
+	def send_mail
+		NewUserMailer.new_user(self).delivery_now
+	end
+
 end
