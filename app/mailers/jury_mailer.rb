898aac2cc9f476e9def6ca4751fab1384fed1695
Jefferson Daniel Castro Aguilar <jedcastroag@unal.edu.co>
2019-03-10 00:53:16 -0500

    email delivery checked

--- app/mailers/jury_mailer.rb
+++ app/mailers/jury_mailer.rb
@@ -1,2 +1,12 @@
 class JuryMailer < ApplicationMailer
+    default from: 'ptesis2019@gmail.com' 
+    Concept_created_subject = "The ThesisProject/Thesis Concept has been Uploaded"
+    @URL = "localhost:5000/login"
+
+    def concept_created
+        @user = params[:email]
+        mail(to: @user, subject: Concept_created_subject )
+    end
+    
+
 end
